aa217aa6321716790e0b9e46b36013466b7f53d5
pooja280700 <poojarajeshgupta1@gmail.com>
2021-06-29 17:28:40 +0530

    Added pallindrome

--- pallindrome.py
+++ pallindrome.py
@@ -3,3 +3,8 @@ if(string==string[::-1]):
     print("String is pallindrome")
 else:
     print("it is not pallindrome")
+    print("oooo")
+    print("oooo")
+    print("oooo")
+    print("oooo")
+    print("oooo")
